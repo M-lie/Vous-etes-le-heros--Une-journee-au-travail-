fd72d666047fc5d628c2dd5b517da047a0e1623c
2142692 <89608255+2142692@users.noreply.github.com>
2022-10-16 00:16:44 -0400

    super

--- desy_maika_PS1_582-324MO/game.js
+++ desy_maika_PS1_582-324MO/game.js
@@ -5,101 +5,101 @@ let chaptersObj = {
         subtitle: 'réveille',
         text:"Vous vous réveillez en retard pour votre journée de travail. Voulez-vous vous dépêcher ou y allez lentement?",
         img: "./assets/lit.jpg",
-        options:{choix1 :{texte: "se dépêcher", action:"goToChapter('dépêcher')"},
-        choix2 :{texte: "lentement", action:"goToChapter('lentement')"},},},
+        options:[{texte: "se dépêcher", action:"goToChapter('dépêcher')"},
+        {texte: "lentement", action:"goToChapter('lentement')"},]},
 
     dépêcher : {
         subtitle: "dépêcher",
         text:"Vous tébuchez dans les escaliers et vous mourrez.",
         img: "./assets/escaliers.jpg",
-        options: { choix1 :{texte:"se réveiller", action: "goToChapter('intro')"},},},
+        options: [{texte:"se réveiller", action: "goToChapter('intro')"},]},
 
      lentement : {
         subtitle: "cuisine",
         text:"vous arrivez dans la cuisine et vous êtes en retard. Voulez-vous mangez? Que faites-vous?",
         img: "./assets/cuisine.jpg",
-        options: { choix1 : {texte:"pas manger", action: "goToChapter(pasmanger)"},
-    choix2: {texte:"manger", action: "goToChapter('manger')"},
-choix3: { texte:"les deux", action: "goToChapter(deux)"},},},
+        options:  [{texte:"pas manger", action: "goToChapter(pasmanger)"},
+     {texte:"manger", action: "goToChapter('manger')"},
+ { texte:"les deux", action: "goToChapter(deux)"},]},
 
     pasmanger : {
         subtitle: "famine",
         text:"Vous mourez de faim.",
         img: "./assets/famine.jpg",
-        options: { choix1 :{texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [{texte: "se réveiller", action: "goToChapter('intro')"},]
     },
 
     manger : {
         subtitle: "manger",
         text:"Vous vous étouffez.",
         img: "./assets/étouffer.jpg",
-        options: { choix1 : {texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [{texte: "se réveiller", action: "goToChapter('intro')"},]
     },
 
     deux : {
         subtitle: "transport1",
         text:"Vous partez en même temps que manger et vous décidez de prendre un transport.",
         img: "./assets/transport.jpg",
-        options: { choix1 :{ texte: "autobus", action: "goToChapter('autobus')"},
-    choix2: { texte:"hélicoptère", action: "goToChapter('hélicoptère')"},
-choix3: { texte:"métro", action: "goToChapter('metro')"},},},
+        options: [{ texte: "autobus", action: "goToChapter('autobus')"},
+    { texte:"hélicoptère", action: "goToChapter('hélicoptère')"},
+ { texte:"métro", action: "goToChapter('metro')"},]},
 
     autobus : {
         subtitle: "autobus",
         text:"Vous vous faites écrasez par l'autobus.",
         img: "./assets/autobus.jpg",
-        options: { choix1 : {texte:"se réveiller", action: "goToChapter('intro')"},},},
+        options:[ {texte:"se réveiller", action: "goToChapter('intro')"},]},
 
     hélicoptère : {
         subtitle: "hélicoptère",
         text:"Vous prenez l'hélicoptère et vous arrivez au travail. Vous voyez vos collègues. Que faites-vous?",
         img: "./assets/hélicoptère.jpg",
-        options: { choix1 : {texte:"ignorer", action: "goToChapter('ignorer')"},
-    choix2: {texte:"saluer", action: "goToChapter('saluer')"},},},
+        options: [ {texte:"ignorer", action: "goToChapter('ignorer')"},
+     {texte:"saluer", action: "goToChapter('saluer')"},]},
 
     metro : {
         subtitle: "métro",
         text:"Vous prenez le métro et vous arrivez au travail. Vous voyez vos collègues. Que faites-vous?",
         img: "./assets/métro.jpg",
-        options: { choix1 :{ texte:"ignorer", action: "goToChapter('ignorer')"},
-        choix2: {texte:"saluer", action: "goToChapter('saluer')"},},
+        options:[{ texte:"ignorer", action: "goToChapter('ignorer')"},
+         {texte:"saluer", action: "goToChapter('saluer')"},]
     },
 
     ignorer : {
         subtitle: "ignorer",
         text:"Vous allez directement à votre bureau sans saluer votre collègue. Voulez-vous faire des appels ou travaillez sur votre ordinateur?",
         img: "./assets/bureau.jpg",
-        options: { choix1 : {texte:"téléphone", action: "goToChapter('téléphone')"},
-    choix2: { texte:"ordinateur", action: "goToChapter('ordinateur')"},},
+        options: [ {texte:"téléphone", action: "goToChapter('téléphone')"},
+     { texte:"ordinateur", action: "goToChapter('ordinateur')"},]
     },
 
     téléphone : {
         subtitle: "téléphone",
         text:"Vous prenez le téléphone maladroitement et vous vous étranglez avec le fil du téléphone.",
         img: "./assets/fil.jpg",
-        options: { choix1 : {texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [{texte: "se réveiller", action: "goToChapter('intro')"},]
     },
 
     ordinateur : {
         subtitle: "ordinateur",
         text:"Pendant que vous travaillez sur votre ordinateur, vos collègues vous lance des boules de papiers et vous mourrez",
         img: "./assets/papiers.jpg",
-        options: { choix1 : {texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [{texte: "se réveiller", action: "goToChapter('intro')"},]
     },
 
     saluer : {
         subtitle: "saluer",
         text:"Vous saluez vos collègues et vous vous faites plein d'amis. Vous avez tellement une belle attitude que votre patron vous appel à son bureau. Vous arrivez à son bureau pour parler.",
         img: "./assets/collegue.jpg",
-        options: { choix1 : {texte:"blague", action: "goToChapter('blague')"},
-    choix2: {texte:"sérieux", action: "goToChapter('sérieux')"},},
+        options:[ {texte:"blague", action: "goToChapter('blague')"},
+    {texte:"sérieux", action: "goToChapter('sérieux')"},]
     },
 
     blague : {
         subtitle: "blague",
         text:"Vous décidez de commencez la discussion par une blague. C'était tellement pas drôle que vous mourez de malaise.",
         img: "./assets/malaise.jpg",
-        options: { choix1 : {texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [{texte: "se réveiller", action: "goToChapter('intro')"},]
     },
 
     sérieux : {
@@ -112,36 +112,36 @@ choix3: { texte:"métro", action: "goToChapter('metro')"},},},
         subtitle: "autobus2",
         text:"Vous prenez l'autobus et il tombe dans un ravin",
         img: "./assets/ravin.jpg",
-        options: { choix1 :{texte: "se réveiller", action: "goToChapter('intro')"},},
+        options: [{texte: "se réveiller", action: "goToChapter('intro')"},]
     },
 
     hélicoptère2: {
         subtitle: "hélicoptère2",
         text:"L'hélicoptère manque de carburant et vous vous écrasez",
         img: "./assets/explosion.jpg",
-        options: { choix1 : {texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [ {texte:"se réveiller", action: "goToChapter('intro')"},]
     },
 
     métro2: {
         subtitle: "metro2",
         text:"vous prenez le métro et vous arrivez chez vous après une longue journée au travail. Vos collègues vous appel pour vous inviter à une fête. Voulez-vous y allez ou vous reposer chez vous?",
         img: "./assets/salon.jpg",
-        options: { choix1 :{texte:"dormir", action: "goToChapter('dormir')"},
-    choix2: {texte:"aller à la fête", action: "goToChapter('fête')"},},
+        options: [{texte:"dormir", action: "goToChapter('dormir')"},
+     {texte:"aller à la fête", action: "goToChapter('fête')"},]
     },
 
     dormir: {
         subtitle: "dormir",
         text:"Vous décidez de dormir plus tôt au lieu d'allez à la fête, mais vous culpabilisez et vous mourez.",
         img: "./assets/culpabilise.jpg",
-        options: { choix1 : {texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [{texte:"se réveiller", action: "goToChapter('intro')"},]
     },
 
     fête :{
         subtitle: "fête",
         text:"Vous décidez d'aller à la fête et vous vous amusez. Ne vous coucher pas trop tard demain vous retravaillez.",
         img: "./assets/fête.jpg",
-        options: { choix1 : { texte:"se réveiller", action: "goToChapter('intro')"},},
+        options: [ { texte:"se réveiller", action: "goToChapter('intro')"},]
     },
 
 };
@@ -149,16 +149,20 @@ choix3: { texte:"métro", action: "goToChapter('metro')"},},},
 
 
 function goToChapter(chapterName){
+    console.log(chaptersObj[chapterName]["subtitle"]);
+    console.log(chaptersObj[chapterName]["text"]);
     document.querySelector(".titre").innerHTML=chaptersObj[chapterName]["subtitle"];
     document.querySelector(".text").innerHTML= chaptersObj[chapterName]["text"];
-    document.querySelector(".travail").innerHTML=`<img src="${chaptersObj[chapterName]["img"]}" class="image">`
-    document.querySelector(".choix no1").innerHTML= chaptersObj[chapterName]["options"][0]["choix1"]["texte"];
-    document.querySelector(".choix no2").innerHTML= chaptersObj[chapterName]["options"][0]["choix2"]["texte"];
-    document.querySelector(".choix no3").innerHTML= chaptersObj[chapterName]["options"][0]["choix3"]["texte"];
+    document.querySelector(".imgchanger").innerHTML=`<img src="${chaptersObj[chapterName]["img"]}" class="travail">`;
+    document.querySelector(".choix .no1").innerHTML= chaptersObj[chapterName]["options"][0]["texte"];
+    document.querySelector(".choix .no2").innerHTML= chaptersObj[chapterName]["options"][1]["texte"];
+    document.querySelector(".choix .no3").innerHTML= chaptersObj[chapterName]["options"][2]["texte"];
+    for(i = 0; i < chaptersObj[chapterName].options.lenght; i++) {
+        <button class="choix no1" onclick ="$[chaptersObj[chapterName].options[i].action]">$[chaptersObj[chapterName].options[i].choix1]</button>;
+        <button class="choix no2" onclick ="$[chaptersObj[chapterName].options[i].action]">$[chaptersObj[chapterName].options[i].choix2]</button>;
+        <button class="choix no3" onclick ="$[chaptersObj[chapterName].options[i].action]">$[chaptersObj[chapterName].options[i].choix3]</button>;
+    }
+    
+
 };
 
-function click(){
-    chaptersObj[chapterName]["options"][0]["choix1"]["action"];
-    chaptersObj[chapterName]["options"][0]["choix2"]["action"];
-    chaptersObj[chapterName]["options"][0]["choix3"]["action"];
-}
